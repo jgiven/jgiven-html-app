c4f28ecc20acb181b1458690b11f3dfb0d823151
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-02-24 13:39:55 +0100

    WIP: implement cases table

--- new/src/App.test.tsx
+++ new/src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+//Nice wrapper library for testing https://testing-library.com/docs/ecosystem-user-event/
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
