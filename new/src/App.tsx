cbae50891296a3dea352da467a46838216c83ac8
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-02-24 08:56:16 +0100

    Move code to legacy folder and create new react folder next to it

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,33 +1,26 @@
-import "./App.css";
-import ReportModel, { ReportStatistics } from "./reportModel";
-import * as guaranteedStateScenario from "./sampleData/GuaranteedStateTestScenario.json";
-import { SingleScenarioView } from "./components/Scenarios/SingleScenarioView";
-
-export const statistics: ReportStatistics[] = [
-    {
-        numClasses: 3,
-        numScenarios: 5,
-        numFailedScenarios: 2,
-        numCases: 3,
-        numFailedCases: 3,
-        numSteps: 10,
-        durationInNanos: 12345678910,
-        numPendingScenarios: 0,
-        numSuccessfulScenarios: 3
-    }
-];
-
-const guaranteedStateReport: ReportModel = guaranteedStateScenario as unknown as ReportModel;
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className="App" aria-label="App">
-            <SingleScenarioView
-                reportName={guaranteedStateReport.name}
-                scenario={guaranteedStateReport.scenarios[0]}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
