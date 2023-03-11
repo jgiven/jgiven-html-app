a26240a125eed56cd8a159b37dcb3cc2862cddbe
Christian Oertel <christian.oertel@tngtech.com>
2023-03-11 16:23:08 +0100

    Provide donut diagram

--- new/src/components/ScenarioOverview/ScenarioOverview.tsx
+++ new/src/components/ScenarioOverview/ScenarioOverview.tsx
@@ -1,11 +1,60 @@
 import type {ReportStatistics} from "../../reportModel";
-import {Breadcrumbs, Link, Typography} from "@mui/material";
+import {Breadcrumbs, Divider, Link, List, ListItem, ListItemText, Typography} from "@mui/material";
 import CheckIcon from '@mui/icons-material/CheckBox';
 import ErrorIcon from '@mui/icons-material/Error';
 import DoNotDisturbAltIcon from '@mui/icons-material/DoNotDisturbAlt';
 import {addRuntime} from "../utils";
+import DonutChart from "react-donut-chart";
 
 export function ScenarioOverview(props: { statistic: ReportStatistics }) {
+    return (
+        <List sx={undefined} component="nav" aria-label="jgiven-overview">
+            <ListItem button>
+                <ListItemText primary={createReportCircle(props)} />
+            </ListItem>
+            <Divider />
+            <ListItem button>
+                <ListItemText primary={getBreadcrumbs(props)} />
+            </ListItem>
+        </List>
+    )
+}
+
+// function getTopLevel() {
+//     return (
+//         <Typography variant="h4">All Scenarios</Typography>
+//     )
+// }
+
+function createReportCircle(props: {statistic: ReportStatistics}) {
+    const reactDonutChartdata = [
+        {
+            label: "Success: ",
+            value: props.statistic.numSuccessfulScenarios,
+            color: "#00E396"
+        },
+        {
+            label: "Failed: ",
+            value: props.statistic.numFailedScenarios,
+            color: "#FEB019"
+        }
+    ];
+    return (
+        <DonutChart
+            width={120}
+            height={120}
+            data={reactDonutChartdata}
+            colors={["green", "red"]}
+            innerRadius={0.55}
+            selectedOffset={0}
+            legend={false}
+            clickToggle={false}
+            interactive={false}
+        />
+    );
+}
+
+function getBreadcrumbs(props: { statistic: ReportStatistics}) {
     return (
         <Breadcrumbs separator=" " aria-label="breadcrumb">
             <Link underline="hover" color={"inherit"} href={"/TODO"}>
