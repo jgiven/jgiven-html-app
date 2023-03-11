16501ed4c497d87dfd2828d15eaa902d2b137fe7
Christian Oertel <christian.oertel@tngtech.com>
2023-03-11 10:29:08 +0100

    Add remaining ReportModel Interfaces

--- new/src/reportModel.d.ts
+++ new/src/reportModel.d.ts
@@ -1,6 +1,58 @@
 export default interface ReportModel {
 
 }
+interface ArgumentInfo {
+    parameterName: string,
+    argumentName: string,
+    formattedValue?: string,
+    dataTable?: DataTable,
+}
+interface AttachmentModel {
+    title:string,
+    value:string,
+    fileName:string,
+    mediaType: string,
+    binary: boolean
+}
+interface CompleteReportModel {
+    models: ReportModelFile[],
+    tagMap: Map<Tag, ScenarioModel[]>,
+    statisticsMap: Map<ReportModelFile, ReportStatistics>,
+    totalStatistics: ReportStatistics,
+    failedScenarios: ScenarioModel[],
+    pendingScenarios: ScenarioModel[],
+    allScenarios: ScenarioModel[],
+    tagIdMap: Map<string, Tag>,
+}
+interface DataTable {
+    headerType: HeaderType
+    data: string[][]
+}
+interface ExecutionStatusCalculator {
+    failedCount: number,
+    pendingCount: number,
+    totalCount: number,
+    status: ExecutionStatus
+}
+interface NamedArgument {
+    name: string,
+    value: object,
+}
+interface ReportModelFile {
+    model: ReportModel,
+    file: File
+}
+interface ReportStatistics {
+    numClasses: number,
+    numScenarios: number,
+    numFailedScenarios: number,
+    numCases: number,
+    numFailedCases: number,
+    numSteps: number,
+    durationInNanos: number,
+    numPendingScenarios: number,
+    numSuccessfulScenarios: number
+}
 interface ScenarioCaseModel {
     caseNr: number,
     steps: StepModel[],
@@ -10,7 +62,27 @@ interface ScenarioCaseModel {
     errorMessage?: string,
     stackTrace?: string[],
     durationInNanos: number,
+    description?: string,
+
+}
+interface ScenarioModel {
+    className: string,
+    testMethodName: string,
     description: string,
+    extendedDescription: string,
+    tagIds: string[],
+    explicitParameters: string[],
+    derivedParameters: string[],
+    scenarioCases: ScenarioCaseModel[],
+    casesAsTable: boolean,
+    durationInNanos: number,
+    executionStatus: ExecutionStatus,
+
+}
+interface StepFormatter {
+    stepDescription: string,
+    arguments: NamedArgument[],
+    formatters,
 
 }
 interface StepModel {
@@ -21,42 +93,32 @@ interface StepModel {
     durationInNanos: number,
     extendedDescription?: string,
     attachments?: AttachmentModel[],
-    isSeciontTitle?: boolean,
+    isSectionTitle?: boolean,
     comment?: string,
     depth: number,
     parentFailed: boolean,
 }
-
-interface AttachmentModel {
-    title:string,
-    value:string,
-    fileName:string,
-    mediaType: string,
-    binary: boolean
+interface Tag {
+    fullType: string,
+    type: string,
+    name: string,
+    value: object,
+    description: string,
+    prependType: boolean,
+    color: string,
+    cssClass: string,
+    style: string,
+    tags: string[],
+    href: string,
+    hideInNav: boolean,
 }
-
 interface Word {
     value: string,
     isIntroWord: boolean,
     argumentInfo?: ArgumentInfo,
 
 }
-
-interface ArgumentInfo {
-    parameterName: string,
-    argumentName: string,
-    formattedValue?: string,
-    dataTable?: DataTable,
-    isDifferent?:boolean
-
-
-}
-
-interface DataTable {
-   headerType: HeaderType
-    data: string[][]
-}
-
-type HeaderType = "NONE"| "HORIZONTAL"|"VERTICAL" | "BOTH";
-type StepStatus = "PASSED" | "FAILED" | "SKIPPED" | "PENDING";
 type ExecutionStatus = "SCENARIO_PENDING" | "SUCCESS" | "FAILED" | "SOME_STEPS_PENDING";
+type HeaderType = "NONE"| "HORIZONTAL"|"VERTICAL" | "BOTH";
+type InvocationMode = "NORMAL" |"NESTED" | "FAILED"| "SKIPPED" | "PENDING";
+type StepStatus = "PASSED" | "FAILED" | "SKIPPED" | "PENDING";
