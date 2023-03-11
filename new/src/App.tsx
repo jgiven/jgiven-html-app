5936924f6cfca46b5923a7b9d58410289a755660
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-03-11 11:13:21 +0100

    Rename scenario case

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 //import {CasesTable} from "./components/casesTable/CasesTable";
 import type {ScenarioCaseModel} from './reportModel';
-import guaranteedStatScenario from '../sampleData/GuaranteedStateTestScenario.json'
-import {ScenarioCase} from "./components/Scenario";
+import guaranteedStatScenario from './sampleData/GuaranteedStateTestScenario.json'
+import {ScenarioCase} from "./components/ScenarioCase";
 
 const cases: ScenarioCaseModel[] = [
     {
@@ -18,22 +17,10 @@ const cases: ScenarioCaseModel[] = [
 ]
 
 function App() {
+    let a = cases;
+    a = a;
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
             <ScenarioCase scenarioCase={guaranteedStatScenario.scenarios[0].scenarioCases[0]}/>
             {/*<CasesTable cases={cases} */}
         </div>
