c4b30967aaada27ce487f199c235dc608d145284
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-03-11 14:04:06 +0100

    WIP

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,27 +1,11 @@
 import './App.css';
-//import {CasesTable} from "./components/casesTable/CasesTable";
 import type ReportModel from './reportModel';
-import type {ScenarioCaseModel} from './reportModel';
 import guaranteedStateScenario from './sampleData/GuaranteedStateTestScenario.json'
 import {Scenario} from "./components/Scenario/Scenario";
-
-const cases: ScenarioCaseModel[] = [
-    {
-        caseNr: 0,
-        derivedArguments: [],
-        description: "A sample Case",
-        durationInNanos: 12345556,
-        explicitArguments:[],
-        status: "SUCCESS",
-        steps:[],
-    }
-]
 
 const guaranteedStateReport: ReportModel = guaranteedStateScenario as unknown as ReportModel;
 
 function App() {
-    let a = cases;
-    a = a;
     return (
         <div className="App">
             <Scenario scenario={guaranteedStateReport.scenarios[0]}/>
