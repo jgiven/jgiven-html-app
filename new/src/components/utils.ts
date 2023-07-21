80fbda9af18e44d7ae09aef5526cc8262a12db42
Christian Oertel <christian.oertel@tngtech.com>
2023-07-21 14:02:55 +0200

    Adjust displayed time
    Signed-off-by: Christian Oertel <christian.oertel@tngtech.com>

--- new/src/components/utils.ts
+++ new/src/components/utils.ts
@@ -1,3 +1,5 @@
-export function addRuntime(input: {durationInNanos:number}): string{
-    return input.durationInNanos > 1e7 ? `(${Math.round(input.durationInNanos/1e6)}ms)` : "";
+export function addRuntime(input: {durationInNanos: number}): string{
+    return input.durationInNanos > 1e9 ? `(${(input.durationInNanos / 1e9).toFixed(3)}s)`
+        : "";
+        //`(${Math.round(input.durationInNanos/1e6)}ms)` : "";
 }
