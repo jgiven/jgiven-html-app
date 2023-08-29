4ea6d5e2e15241ff7539661d563f41145a5d47aa
l-1sqared <30831153+l-1squared@users.noreply.github.com>
2023-08-29 07:04:08 +0200

    Allow collective collapse and expansion of accordion

--- new/src/components/Scenarios/Scenario.tsx
+++ new/src/components/Scenarios/Scenario.tsx
@@ -2,29 +2,42 @@ import type {ScenarioCaseModel, ScenarioModel, StepModel} from "../../reportMode
 import {Accordion, AccordionDetails, Box, Grid, Link, Typography} from "@mui/material";
 import {styled} from '@mui/material/styles';
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordionSummary, {
-    AccordionSummaryProps,
-} from '@mui/material/AccordionSummary';
-import {PropsWithChildren, useState} from "react";
+import MuiAccordionSummary, {AccordionSummaryProps,} from '@mui/material/AccordionSummary';
+import {PropsWithChildren} from "react";
 import {addRuntime} from "../utils"
 import {StatusIcon} from "../StatusIconSelector";
 
+interface ScenarioProps {
+    scenario: ScenarioModel;
+    reportName?: string;
+    accordeonExpansion: {
+        expanded: boolean;
+        setExpanded: (expanded: boolean) => void;
+    }
+}
 
-
-export function Scenario(props: { scenario: ScenarioModel, reportName?:string, expanded?: boolean }) {
+export function Scenario(props: ScenarioProps) {
     return props.scenario.scenarioCases.length === 1 ?
         (<SingleCaseScenario
                 scenarioCase={props.scenario.scenarioCases[0]}
                 reportName={props.reportName}
                 summary={props.scenario.description}
-                expanded={props.expanded ?? false}
+                expanded={props.accordeonExpansion.expanded}
+                setExpanded={props.accordeonExpansion.setExpanded}
                 className={props.scenario.className}
             />
         )
         : (<div></div>);
 }
-export function SingleCaseScenario(props: { scenarioCase: ScenarioCaseModel, expanded: boolean, reportName?:string, summary: string, className: string }) {
-    const [expanded, setExpanded] = useState(props.expanded);
+
+export function SingleCaseScenario(props: {
+    scenarioCase: ScenarioCaseModel,
+    expanded: boolean,
+    setExpanded: (expanded: boolean) => void,
+    reportName?: string,
+    summary: string,
+    className: string
+}) {
     const AccordionSummary = styled((props: AccordionSummaryProps) => (
         <MuiAccordionSummary
             expandIcon={<ArrowForwardIosSharpIcon sx={{fontSize: '0.9rem'}}/>}
@@ -45,8 +58,10 @@ export function SingleCaseScenario(props: { scenarioCase: ScenarioCaseModel, exp
     }));
 
     return (
-        <Accordion expanded={expanded} >
-            <AccordionSummary onClick={()=>{setExpanded(!expanded)}}>
+        <Accordion expanded={props.expanded}>
+            <AccordionSummary onClick={() => {
+                props.setExpanded(!props.expanded)
+            }}>
                 <Grid container columnSpacing={1}>
                     <Grid item><Typography color={"grey"}>{props.reportName}</Typography></Grid>
                     <Grid item><Typography>{props.summary}</Typography></Grid>
@@ -58,7 +73,7 @@ export function SingleCaseScenario(props: { scenarioCase: ScenarioCaseModel, exp
                 <Box sx={{"margin-left": '2em'}}>
                     {props.scenarioCase.steps.map((step: StepModel) => (<ScenarioStep step={step}></ScenarioStep>))}
                     <Typography align='right' variant='body2'>
-                        <Link href={`#class/${props.className}`}  variant="inherit" color='inherit' underline='none'>
+                        <Link href={`#class/${props.className}`} variant="inherit" color='inherit' underline='none'>
                             {props.className}
                         </Link>
                     </Typography>
