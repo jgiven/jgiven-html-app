78a8302c20bb048fa286c56147cf967967edde84
Christian Oertel <christian.oertel@tngtech.com>
2024-01-19 16:07:12 +0100

    Fix menu bar
    Signed-off-by: Christian Oertel <christian.oertel@tngtech.com>

--- new/src/App.tsx
+++ new/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReportModel, { ReportStatistics } from "./reportModel";
+import * as guaranteedStateScenario from "./sampleData/GuaranteedStateTestScenario.json";
+import { SingleScenarioView} from "./components/Scenarios/SingleScenarioView";
+
+export const statistics: ReportStatistics[] = [
+  {
+    numClasses: 3,
+    numScenarios: 5,
+    numFailedScenarios: 2,
+    numCases: 3,
+    numFailedCases: 3,
+    numSteps: 10,
+    durationInNanos: 12345678910,
+    numPendingScenarios: 0,
+    numSuccessfulScenarios: 3
+  }
+];
+
+const guaranteedStateReport: ReportModel = guaranteedStateScenario as unknown as ReportModel;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App" aria-label="App">
+        <SingleScenarioView
+            reportName={guaranteedStateReport.name}
+            scenario={guaranteedStateReport.scenarios[0]}
+        />
+      </div>
+  )
 }
 
 export default App;
