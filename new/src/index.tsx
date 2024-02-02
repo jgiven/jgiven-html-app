6585932804b7a46a3196aed866dd831395db893b
Christian Oertel <christian.oertel@tngtech.com>
2024-02-02 14:57:37 +0100

    Include review comments
    Signed-off-by: Christian Oertel <christian.oertel@tngtech.com>

--- new/src/index.tsx
+++ new/src/index.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./i18n/en.json";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+i18n.use(initReactI18next).init({
+    resources: {
+        en: { translation: en }
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+        escapeValue: false
+    }
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
