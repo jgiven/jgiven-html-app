f4d577198d5c98d073106e57e0c509e464b004fd
l-1squared <30831153+l-1squared@users.noreply.github.com>
2024-03-01 16:38:19 +0100

    repository now has a get all scenarios method
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- new/src/repository.ts
+++ new/src/repository.ts
@@ -1,33 +1,40 @@
-import jGivenReport from './sampleData/jgivenReport.json';
-import tagFile from './sampleData/tagFile.json';
-import ReportModel from "./reportModel";
+import jGivenReport from "./sampleData/jgivenReport.json";
+import tagFile from "./sampleData/tagFile.json";
+import ReportModel, { ScenarioModel } from "./reportModel";
 
 export const repository = {
     getReport(): JsonReport {
         return jGivenReport as unknown as JsonReport;
     },
 
+    getAllScenarios(): ScenarioModel[] {
+        return jGivenReport.scenarios
+            .map(r => r as ReportModel)
+            .flatMap((report: ReportModel) => report.scenarios);
+    },
+
     getTags(): TagFile {
         return tagFile;
     }
-}
+};
 
 interface JsonReport {
-    scenarios: ReportModel[],
-    tagFile: TagFile
-};
-type TagFile = { tagTypeMap: Record<string, JsonTagType>, tags: Record<string, JsonTag> }
+    scenarios: ReportModel[];
+    tagFile: TagFile;
+}
+
+type TagFile = { tagTypeMap: Record<string, JsonTagType>; tags: Record<string, JsonTag> };
 
 interface JsonTagType {
-    fullType: string,
-    type: string,
-    description: string,
-    prependType?: boolean,
-    color?: string,
-    href: string
+    fullType: string;
+    type: string;
+    description: string;
+    prependType?: boolean;
+    color?: string;
+    href: string;
 }
 
 interface JsonTag {
-    tagType: string,
-    value?: string,
+    tagType: string;
+    value?: string;
 }
