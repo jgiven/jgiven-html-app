0e6e77809af0361afee44b760eda7ede1b9cc0d4
Bastian Jungnitsch <20986347+marcus-aemilius@users.noreply.github.com>
2024-03-01 10:44:02 +0100

    Add react-router-dom and basic routing example

--- new/src/index.tsx
+++ new/src/index.tsx
@@ -4,8 +4,9 @@ import "./index.css";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./i18n/en.json";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
@@ -22,7 +23,7 @@ i18n.use(initReactI18next).init({
 
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
 
