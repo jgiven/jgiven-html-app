bca41ba31a4bb65995bcac53484a48d85879c0c3
Bastian Jungnitsch <20986347+marcus-aemilius@users.noreply.github.com>
2024-03-01 14:08:39 +0100

    Fix tests
    Signed-off-by: Robert Schüler <robert.schueler@tngtech.com>

--- new/src/components/Scenarios/Scenario.test.tsx
+++ new/src/components/Scenarios/Scenario.test.tsx
@@ -2,59 +2,98 @@ import { render, screen } from "@testing-library/react";
 import { ScenarioModel } from "../../reportModel";
 import { Scenario } from "./Scenario";
 import { processWords } from "../../wordProcessor";
+import { ExpansionState } from "./ScenarioOverview";
+import userEvent from "@testing-library/user-event";
+
+afterEach(() => {
+    jest.resetAllMocks();
+});
+
+const onExpansionCallback = jest.fn();
+const onCollapsionCallback = jest.fn();
 
 describe("Scenario accordion behavior", () => {
-    test("accordeon details are not visible when expanded is set to false", async () => {
-        let expanded = false;
-        const setExpanded = (value: boolean) => {
-            expanded = value;
-        };
-        render(<Scenario scenario={model} accordionExpansion={{ expanded, setExpanded }} />);
+    test("accordion details are not visible when globalExpansionState is COLLAPSED", async () => {
+        render(
+            <Scenario
+                scenario={model}
+                globalExpansionState={ExpansionState.COLLAPSED}
+                onExpansionCallback={onExpansionCallback}
+                onCollapsionCallback={onCollapsionCallback}
+            />
+        );
         const accordion = await screen.findByLabelText("Scenario Overview");
         expect(accordion.attributes.getNamedItem("aria-expanded")?.value).toBe("false");
     });
 
-    test("accordion details are visible when expanded is set to true", async () => {
-        let expanded = true;
-        const setExpanded = (value: boolean) => {
-            expanded = value;
-        };
-        render(<Scenario scenario={model} accordionExpansion={{ expanded, setExpanded }} />);
+    test("accordion details are visible when globalExpansionState is EXPANDED", async () => {
+        render(
+            <Scenario
+                scenario={model}
+                globalExpansionState={ExpansionState.EXPANDED}
+                onExpansionCallback={onExpansionCallback}
+                onCollapsionCallback={onCollapsionCallback}
+            />
+        );
         const accordion = await screen.findByLabelText("Scenario Overview");
         expect(accordion.attributes.getNamedItem("aria-expanded")?.value).toBe("true");
     });
 
-    test("accordion details get visible when clicking on the header", async () => {
-        let expanded = false;
-        const setExpanded = (value: boolean) => {
-            expanded = value;
-        };
-        render(<Scenario scenario={model} accordionExpansion={{ expanded, setExpanded }} />);
-        await screen.findByLabelText("Scenario Overview").then(element => element.click());
-        expect(expanded).toBeTruthy();
+    test("onExpansionCallback is invoked when clicking on the header of a collapsed scenario", async () => {
+        render(
+            <Scenario
+                scenario={model}
+                globalExpansionState={ExpansionState.COLLAPSED}
+                onExpansionCallback={onExpansionCallback}
+                onCollapsionCallback={onCollapsionCallback}
+            />
+        );
+        const scenarioOverview = await screen.findByLabelText("Scenario Overview");
+        userEvent.click(scenarioOverview);
+        expect(onExpansionCallback).toHaveBeenCalled();
+    });
+
+    test("onCollapsionCallback is invoked when clicking on the header of an expanded scenario", async () => {
+        render(
+            <Scenario
+                scenario={model}
+                globalExpansionState={ExpansionState.EXPANDED}
+                onExpansionCallback={onExpansionCallback}
+                onCollapsionCallback={onCollapsionCallback}
+            />
+        );
+        const scenarioOverview = await screen.findByLabelText("Scenario Overview");
+        userEvent.click(scenarioOverview);
+        expect(onCollapsionCallback).toHaveBeenCalled();
     });
 });
 
 test("Scenario displays steps", async () => {
-    let expanded = true;
-    const setExpanded = (value: boolean) => {
-        expanded = value;
-    };
-    render(<Scenario scenario={model} accordionExpansion={{ expanded, setExpanded }} />);
+    render(
+        <Scenario
+            scenario={model}
+            globalExpansionState={ExpansionState.EXPANDED}
+            onExpansionCallback={onExpansionCallback}
+            onCollapsionCallback={onCollapsionCallback}
+        />
+    );
     const textElement = await screen.findByText(
         model.scenarioCases[0].steps[0].words.flatMap(word => word.value).join(" ")
     );
-    expect(textElement).toBeTruthy();
+    expect(textElement).toBeInTheDocument();
 });
 
 test("Scenario capitalizes title", async () => {
-    let expanded = true;
-    const setExpanded = (value: boolean) => {
-        expanded = value;
-    };
-    render(<Scenario scenario={model} accordionExpansion={{ expanded, setExpanded }} />);
-    const textElement = await screen.findByText(tekst => processWords(model.description) === tekst);
-    expect(textElement).toBeTruthy();
+    render(
+        <Scenario
+            scenario={model}
+            globalExpansionState={ExpansionState.EXPANDED}
+            onExpansionCallback={onExpansionCallback}
+            onCollapsionCallback={onCollapsionCallback}
+        />
+    );
+    const textElement = await screen.findByText(processWords(model.description));
+    expect(textElement).toBeInTheDocument();
 });
 
 const model: ScenarioModel = {
