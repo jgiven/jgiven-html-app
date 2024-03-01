b0b3012f58db63e48c92bfb0da382a492b29bdcf
Robert Schüler <robert.schueler@tngtech.com>
2024-03-01 14:35:59 +0100

    number format change

--- new/src/components/utils.ts
+++ new/src/components/utils.ts
@@ -1,4 +1,3 @@
 export function addRuntime(input: { durationInNanos: number }): string {
-    return input.durationInNanos > 1e9 ? `(${(input.durationInNanos / 1e9).toFixed(3)}s)` : "";
-    //`(${Math.round(input.durationInNanos/1e6)}ms)` : "";
+    return input.durationInNanos > 1e7 ? `(${(input.durationInNanos / 1e9).toFixed(3)}s)` : "";
 }
