5fd63b82d6e2b8ddff5a1569b5a380c29df0f572
Robert Schüler <robert.schueler@tngtech.com>
2024-03-01 15:13:17 +0100

    Restructured Scenario Accordion

--- new/src/components/Scenarios/Scenario.tsx
+++ new/src/components/Scenarios/Scenario.tsx
@@ -1,13 +1,11 @@
-import type { ScenarioCaseModel, ScenarioModel, StepModel } from "../../reportModel";
-import { Accordion, AccordionDetails, Box, Grid, Link, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import MuiAccordionSummary, { AccordionSummaryProps } from "@mui/material/AccordionSummary";
-import { PropsWithChildren, useCallback, useEffect, useState } from "react";
-import { addRuntime } from "../utils";
-import { StatusIcon } from "../StatusIconSelector";
-import { processWords } from "../../wordProcessor";
-import { ExpansionState } from "./ScenarioOverview";
+import type {ScenarioCaseModel, ScenarioModel, StepModel} from "../../reportModel";
+import {Accordion, AccordionDetails, Box, Link, Typography} from "@mui/material";
+import {useCallback, useEffect, useState} from "react";
+import {addRuntime} from "../utils";
+import {processWords} from "../../wordProcessor";
+import {ExpansionState} from "./ScenarioOverview";
+import {ScenarioHead} from "./ScenarioHead";
+import {ScenarioCaption} from "./ScenarioCaption";
 
 export interface ScenarioProps {
     scenario: ScenarioModel;
@@ -42,14 +40,23 @@ export function Scenario(props: ScenarioProps) {
             id={`${props.scenario.className}#${props.scenario.testMethodName}`}
             aria-label={`Scenario ${props.scenario.description}`}
         >
-            <SingleCaseScenario
-                scenarioCase={props.scenario.scenarioCases[0]}
-                reportName={props.reportName}
-                summary={props.scenario.description}
-                expanded={expanded}
-                setExpanded={onExpansionChanged}
-                className={props.scenario.className}
-            />
+            <Accordion expanded={expanded}>
+                <ScenarioHead
+                    scenario={props.scenario}
+                    expanded={expanded}
+                    setExpanded={onExpansionChanged}
+                    summary={"Summary"}
+                    className={"className"}
+                />
+                <SingleCaseScenario
+                    scenarioCase={props.scenario.scenarioCases[0]}
+                    reportName={props.reportName}
+                    summary={props.scenario.description}
+                    expanded={expanded}
+                    setExpanded={onExpansionChanged}
+                    className={props.scenario.className}
+                />
+            </Accordion>
         </div>
     ) : (
         <div></div>
@@ -64,64 +71,26 @@ function SingleCaseScenario(props: {
     summary: string;
     className: string;
 }) {
-    const AccordionSummary = styled((props: AccordionSummaryProps) => (
-        <MuiAccordionSummary
-            expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
-            {...props}
-        />
-    ))(({ theme }) => ({
-        backgroundColor:
-            theme.palette.mode === "dark" ? "rgba(255, 255, 255, .05)" : "rgba(0, 0, 0, .03)",
-        flexDirection: "row-reverse",
-        "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-            transform: "rotate(90deg)"
-        },
-        "& .MuiAccordionSummary-content": {
-            marginLeft: theme.spacing(1)
-        }
-    }));
+
 
     return (
-        <Accordion expanded={props.expanded}>
-            <AccordionSummary
-                aria-label="Scenario Overview"
-                onClick={() => {
-                    props.setExpanded(!props.expanded);
-                }}
-            >
-                <Grid container columnSpacing={1}>
-                    <Grid item>
-                        <Typography color={"grey"}>{props.reportName}</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography>{processWords(props.summary)}</Typography>
-                    </Grid>
-                    <Grid>
-                        <StatusIcon model={props.scenarioCase} />
-                    </Grid>
-                    <Grid>
-                        <Caption>{addRuntime(props.scenarioCase)}</Caption>
-                    </Grid>
-                </Grid>
-            </AccordionSummary>
-            <AccordionDetails aria-label="Scenario Steps">
-                <Box sx={{ marginLeft: "2em" }}>
-                    {props.scenarioCase.steps.map((step: StepModel, index) => (
-                        <ScenarioStep key={index} step={step}></ScenarioStep>
-                    ))}
-                    <Typography align="right" variant="body2">
-                        <Link
-                            href={`#class/${props.className}`}
-                            variant="inherit"
-                            color="inherit"
-                            underline="none"
-                        >
-                            {props.className}
-                        </Link>
-                    </Typography>
-                </Box>
-            </AccordionDetails>
-        </Accordion>
+        <AccordionDetails aria-label="Scenario Steps">
+            <Box sx={{marginLeft: "2em"}}>
+                {props.scenarioCase.steps.map((step: StepModel, index) => (
+                    <ScenarioStep key={index} step={step}></ScenarioStep>
+                ))}
+                <Typography align="right" variant="body2">
+                    <Link
+                        href={`#class/${props.className}`}
+                        variant="inherit"
+                        color="inherit"
+                        underline="none"
+                    >
+                        {props.className}
+                    </Link>
+                </Typography>
+            </Box>
+        </AccordionDetails>
     );
 }
 
@@ -129,15 +98,9 @@ function ScenarioStep(props: { step: StepModel }) {
     const stepDescription = processWords(props.step.words);
     return (
         <Typography align={"left"}>
-            {stepDescription} <Caption>{addRuntime(props.step)}</Caption>
+            {stepDescription} <ScenarioCaption>{addRuntime(props.step)}</ScenarioCaption>
         </Typography>
     );
 }
 
-function Caption(props: PropsWithChildren) {
-    return (
-        <Typography display="inline" variant="caption" paragraph={false}>
-            {props.children}
-        </Typography>
-    );
-}
+
---
+++ new/src/components/Scenarios/ScenarioHead.tsx
@@ -0,0 +1,57 @@
+import { Grid, Typography} from "@mui/material";
+import { ScenarioModel} from "../../reportModel";
+import {processWords} from "../../wordProcessor";
+import {StatusIcon} from "../StatusIconSelector";
+import {addRuntime} from "../utils";
+import {styled} from "@mui/material/styles";
+import MuiAccordionSummary, {AccordionSummaryProps} from "@mui/material/AccordionSummary";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import {ScenarioCaption} from "./ScenarioCaption";
+
+export function ScenarioHead(props: {
+    scenario: ScenarioModel;
+    expanded: boolean;
+    setExpanded: (expanded: boolean) => void;
+    reportName?: string;
+    summary: string;
+    className: string;
+}) {
+    const AccordionSummary = styled((props: AccordionSummaryProps) => (
+        <MuiAccordionSummary
+            expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+            {...props}
+        />
+    ))(({ theme }) => ({
+        backgroundColor:
+            theme.palette.mode === "dark" ? "rgba(255, 255, 255, .05)" : "rgba(0, 0, 0, .03)",
+        flexDirection: "row-reverse",
+        "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+            transform: "rotate(90deg)"
+        },
+        "& .MuiAccordionSummary-content": {
+            marginLeft: theme.spacing(1)
+        }
+    }));
+
+    return <AccordionSummary
+        aria-label="Scenario Overview"
+        onClick={() => {
+            props.setExpanded(!props.expanded);
+        }}
+    >
+        <Grid container columnSpacing={1}>
+            <Grid item>
+                <Typography color={"grey"}>{props.reportName}</Typography>
+            </Grid>
+            <Grid item>
+                <Typography>{processWords(props.summary)}</Typography>
+            </Grid>
+            <Grid>
+                <StatusIcon executionStatus={props.scenario.executionStatus} />
+            </Grid>
+            <Grid>
+                <ScenarioCaption>{addRuntime(props.scenario.scenarioCases[0])}</ScenarioCaption>
+            </Grid>
+        </Grid>
+    </AccordionSummary>
+}
