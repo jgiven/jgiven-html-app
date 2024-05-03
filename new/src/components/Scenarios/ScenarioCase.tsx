cd06bc6e1499392a42434956d61ac094fb202e33
Robert Schüler <robert.schueler@tngtech.com>
2024-05-03 10:59:20 +0200

    Refactored Scenario component
    - moved all Accordion controls to the Scenario component
    - unified syntax for component properties
    - Put sub-components into dedicated files
    - Removed (currently) unnecessary properties to avoid confusion

---
+++ new/src/components/Scenarios/ScenarioCase.tsx
@@ -0,0 +1,32 @@
+import {Box, Link, Typography} from "@mui/material";
+import {ScenarioCaseModel, StepModel} from "../../reportModel";
+import {ScenarioStep} from "./ScenarioStep";
+
+export interface ScenarioCaseProps {
+    className: string,
+    scenarioCase: ScenarioCaseModel,
+}
+
+export function ScenarioCase(
+    {
+        scenarioCase,
+        className,
+    }: ScenarioCaseProps) {
+    return (
+        <Box sx={{marginLeft: "2em"}}>
+            {scenarioCase.steps.map((step: StepModel, index) => (
+                <ScenarioStep key={index} step={step}></ScenarioStep>
+            ))}
+            <Typography align="right" variant="body2">
+                <Link
+                    href={`#class/${className}`}
+                    variant="inherit"
+                    color="inherit"
+                    underline="none"
+                >
+                    {className}
+                </Link>
+            </Typography>
+        </Box>
+    );
+}
