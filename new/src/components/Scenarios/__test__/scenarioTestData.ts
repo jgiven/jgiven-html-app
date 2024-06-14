9657cc78a581df5a189d319c895011b648456b7c
Robert Schüler <robert.schueler@tngtech.com>
2024-06-14 12:06:32 +0200

    Covered `ScenarioCase` with tests
    Signed-off-by: Robert Schüler <robert.schueler@tngtech.com>

--- new/src/components/Scenarios/__test__/scenarioTestData.ts
+++ new/src/components/Scenarios/__test__/scenarioTestData.ts
@@ -1,11 +1,11 @@
-import {StepModel, Word} from "../../../reportModel";
+import { ScenarioCaseModel, StepModel, Word } from "../../../reportModel";
 
 export function createWord(props?: Partial<Word>): Word {
     return {
         value: props?.value ?? "word value",
         isIntroWord: props?.isIntroWord,
-        argumentInfo: props?.argumentInfo,
-    }
+        argumentInfo: props?.argumentInfo
+    };
 }
 
 export function createStepModel(props?: Partial<StepModel>): StepModel {
@@ -20,6 +20,20 @@ export function createStepModel(props?: Partial<StepModel>): StepModel {
         extendedDescription: props?.extendedDescription,
         attachments: props?.attachments,
         isSectionTitle: props?.isSectionTitle,
-        comment: props?.comment,
-    }
+        comment: props?.comment
+    };
+}
+
+export function createScenarioCaseModel(props?: Partial<ScenarioCaseModel>): ScenarioCaseModel {
+    return {
+        caseNr: props?.caseNr ?? 0,
+        steps: props?.steps ?? [],
+        explicitArguments: props?.explicitArguments ?? [],
+        derivedArguments: props?.derivedArguments ?? [],
+        status: props?.status ?? "SUCCESS",
+        errorMessage: props?.errorMessage,
+        stackTrace: props?.stackTrace,
+        durationInNanos: props?.durationInNanos ?? 0,
+        description: props?.description
+    };
 }
