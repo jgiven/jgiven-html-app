014527792e1a141d3b10eba1b4085eaf555ea34e
l-1squared <30831153+l-1squared@users.noreply.github.com>
2025-03-21 16:31:25 +0100

    chore(JGIVEN-00): prettier everything
    Signed-off-by: l-1squared <30831153+l-1squared@users.noreply.github.com>

--- new/config/jest/cssTransform.js
+++ new/config/jest/cssTransform.js
@@ -6,17 +6,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 // @remove-on-eject-end
-'use strict';
+"use strict";
 
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  process() {
-    return 'module.exports = {};';
-  },
-  getCacheKey() {
-    // The output is always the same.
-    return 'cssTransform';
-  },
+    process() {
+        return "module.exports = {};";
+    },
+    getCacheKey() {
+        // The output is always the same.
+        return "cssTransform";
+    }
 };
