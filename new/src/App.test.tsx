932dec7e3f2b606dd88b1679f1acc9be6da7542e
Robert Schüler <robert.schueler@tngtech.com>
2025-06-24 07:53:43 +0200

    Removed skipped tests
    Signed-off-by: Robert Schüler <robert.schueler@tngtech.com>

--- new/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test.skip("renders learn react link", () => {
-    render(<App />);
-    const app = screen.getByLabelText("App");
-    expect(app).toBeInTheDocument();
-});
