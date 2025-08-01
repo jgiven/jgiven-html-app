dc4b0be5781755579d5b52a9e5de2cae0faefd06
Robert Schüler <robert.schueler@tngtech.com>
2025-08-01 16:22:03 +0200

    Deleted incorrect tests
    Signed-off-by: Robert Schüler <robert.schueler@tngtech.com>

--- new/src/components/Scenarios/__test__/ScenarioStep.test.tsx
+++ new/src/components/Scenarios/__test__/ScenarioStep.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { createStepModel, createWord } from "./scenarioTestData";
 import { ScenarioStep } from "../ScenarioStep";
 import { describe, it, expect } from "vitest";
@@ -17,42 +17,4 @@ describe("ScenarioStep", () => {
 
         expect(screen.getByText(expectedDisplayValue)).toBeVisible();
     });
-
-    it.each([
-        [1e7 + 1, "(0.010s)"],
-        [1e9, "(1.000s)"],
-        [234123455532, "(234.123s)"]
-    ])(
-        "should display the runtime in seconds if durationInNanos = %s",
-        (durationInNanos, expectedDisplayValue) => {
-            const word = "some word";
-            render(
-                <ScenarioStep
-                    step={createStepModel({
-                        words: [createWord({ value: word })],
-                        durationInNanos
-                    })}
-                />
-            );
-
-            expect(screen.getByText(expectedDisplayValue)).toBeVisible();
-        }
-    );
-
-    it.each([[-1e16], [0], [100], [1e7]])(
-        "should not display the duration if durationInNanos = %s",
-        durationInNanos => {
-            const word = "some word";
-            render(
-                <ScenarioStep
-                    step={createStepModel({
-                        words: [createWord({ value: word })],
-                        durationInNanos
-                    })}
-                />
-            );
-
-            expect(within(screen.getByText(word)).getByText("")).toBeInTheDocument();
-        }
-    );
 });
